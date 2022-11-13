1527b668b290043a7132fadc4b08e2ccd4dc9358
abhinand v k <abhinandazokdio@gmail.com>
2022-11-13 07:41:10 +0530

    commit message

--- src/RingToneTypes.ts
+++
@@ -1,8 +0,0 @@
-interface TypeofRingTone {
-  TYPE_ALARM: string;
-  TYPE_ALL: string;
-  TYPE_NOTIFICATION: string;
-  TYPE_RINGTONE: string;
-}
-
-export default TypeofRingTone;
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,18 @@
-export { default as RingtoneManager } from './RingToneManger';
+import { NativeModules } from 'react-native';
+
+const { RingtoneManager } = NativeModules;
+
+type RingtonType =
+  | 'TYPE_ALL'
+  | 'TYPE_RINGTONE'
+  | 'TYPE_NOTIFICATION'
+  | 'TYPE_ALARM';
+
+interface RingtoneManagerInterface {
+  getRingtones(): any;
+  getRingsByType(type: RingtonType): any;
+  pickRingtone(type: RingtonType): string | null;
+  type: RingtonType;
+}
+
+export default RingtoneManager as RingtoneManagerInterface;
